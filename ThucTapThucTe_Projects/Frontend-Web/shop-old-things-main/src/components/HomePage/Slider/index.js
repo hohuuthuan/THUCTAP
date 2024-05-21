ac81202a1cde426a3d90727fa77a5581d65a79fe
HoHuuThuan <thuanb2107182student.ctu.edu.vn>
2024-05-21 10:05:06 +0700

    Chuyển động slide

--- ThucTapThucTe_Projects/Frontend-Web/shop-old-things-main/src/components/HomePage/Slider/index.js
+++ ThucTapThucTe_Projects/Frontend-Web/shop-old-things-main/src/components/HomePage/Slider/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Slider.module.scss';
 
@@ -5,39 +6,39 @@ import slide1 from '~/assets/images/slide-2.jpg';
 import slide2 from '~/assets/images/slide-4.jpg';
 import slide3 from '~/assets/images/slide-6.jpg';
 import slide4 from '~/assets/images/slide-5.jpg';
-import { useEffect, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Slider() {
-    const [slide, setSlide] = useState([slide1, slide2, slide3, slide4]);
-    const [active, setActive] = useState(slide[0]);
-    const [timer, setTimer] = useState(0);
+    const slides = [slide1, slide2, slide3, slide4];
+    const [activeIndex, setActiveIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setTimer((prev) => prev + 1);
+            setActiveIndex((prevIndex) => (prevIndex + 1) % slides.length);
         }, 3000);
 
         return () => {
             clearInterval(interval);
         };
-    }, []);
-
-    useEffect(() => {
-        if (timer === 4) {
-            setTimer(0);
-        }
-
-        setActive(slide[timer]);
-    }, [slide, timer]);
+    }, [slides.length]);
 
     return (
         <div className={cx('wrap')}>
-            <img className={cx('img')} src={active} alt="" />
+            {slides.map((slide, index) => (
+                <img
+                    key={index}
+                    className={cx('img', { active: index === activeIndex })}
+                    src={slide}
+                    alt=""
+                />
+            ))}
             <div className={cx('list')}>
-                {slide.map((item, index) => (
-                    <div key={index} className={cx('item', timer === index && 'active')}></div>
+                {slides.map((_, index) => (
+                    <div
+                        key={index}
+                        className={cx('item', { active: index === activeIndex })}
+                    ></div>
                 ))}
             </div>
         </div>
